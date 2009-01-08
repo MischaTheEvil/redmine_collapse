6ddd1cbe5eda15d5306900b7d73edd6c232b3dcb
MischaTheEvil <mischa_the_evil@hotmail.com>
2009-01-08 21:55:12 +0000

    Cleanup without functional changes.
    git-svn-id: svn://evil-dev.net:3690/redmine-collapse/trunk@10 1809a232-8a5f-0410-be6b-a332444e9cb2

--- lib/collapse_application_helper_patch.rb
+++ lib/collapse_application_helper_patch.rb
@@ -12,10 +12,39 @@ module CollapseApplicationHelperPatch
   end
   
   module InstanceMethods
-  
-    # Returns the tabs-variable containing an array of the tabs to render
+    
+    ###
+    # Compatibility validations
+    ###
+    
+    # Redmine Core 0.8-stable@r2229 (0.8.1)
+    # Returns true if the method is defined, else it returns false
+    def jump_to_current_view_implemented
+      if Redmine::MenuManager::MenuController.method_defined? "redirect_to_project_menu_item"
+        return true
+      else
+        return false
+      end
+    end
+    
+    # Redmine Core 0.8-stable@r2230 (0.8.1)
+    # Returns true if the method is defined, else it returns false
+    def render_flash_messages_implemented 
+      if ApplicationHelper.method_defined? "render_flash_messages" 
+        return true
+      else
+        return false
+      end
+    end
+    
+    ###
+    # Tab renderers
+    ###
+    
+    # Left-menu tab renderer
+    # Returns the tabs-variable containing an array of the left-menu tabs to render
     def left_menu_tabs
-      # Don't render the projects-tab unless specificly configured
+      # Don't render the projects-tab unless specifically configured
       if Setting.plugin_redmine_collapse['show_projects_tab'] == '1'
         tabs = [ { :name => 'actions', :label => l(:label_actions_tab), :partial => 'left_menu/actions.rhtml' },
                  { :name => 'projects', :label => l(:label_projects_tab), :partial => 'left_menu/projects.rhtml' } ]
@@ -25,24 +54,6 @@ module CollapseApplicationHelperPatch
       return tabs
     end
     
-    # Returns true if the required method is defined, else it returns false
-    def jump_to_current_view_implemented
-      if Redmine::MenuManager::MenuController.method_defined? "redirect_to_project_menu_item"
-        return true
-      else
-        return false
-      end
-    end
-    
-    # Returns true if the method is defined, else it returns false
-    def render_flash_messages_implemented 
-      if ApplicationHelper.method_defined? "render_flash_messages" 
-        return true
-      else
-        return false
-      end
-    end
-  
   end
 
 end
