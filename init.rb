5db017d066c2a65865fb9796792d293413f65809
MischaTheEvil <mischa_the_evil@hotmail.com>
2009-01-09 03:28:10 +0000

    Corrected faulty version from @0.0.1@ to @0.1.0@.
    This commit references issue #178.
    
    git-svn-id: svn://evil-dev.net:3690/redmine-collapse/trunk@21 1809a232-8a5f-0410-be6b-a332444e9cb2

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_collapse do
   name 'Redmine Collapse plugin'
   author 'Mischa The Evil'
   description 'This is a plugin for Redmine which transforms the static sidebar into a collapsible sidebar'
-  version '0.0.1'
+  version '0.1.0'
   
   requires_redmine :version_or_higher => '0.8.0'
   
