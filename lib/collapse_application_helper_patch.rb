f2b0d99c86696820289fb7e7be6fb0711b228955
MischaTheEvil <mischa_the_evil@hotmail.com>
2009-04-21 22:12:32 +0000

    Merged sandbox-branch (source:sandbox/140409_1033-MenusTab) for the implementation of the new menus tab with backwards compatibility for the 0.8-stable branch and with the modifications of the projects-tab according to the design of the new menus tab.
    This commit resolves issue #180, #244 & #245.
    
    
    git-svn-id: svn://evil-dev.net:3690/redmine-collapse/trunk@94 1809a232-8a5f-0410-be6b-a332444e9cb2

--- lib/collapse_application_helper_patch.rb
+++ lib/collapse_application_helper_patch.rb
@@ -60,13 +60,15 @@ module CollapseApplicationHelperPatch
     # Tab helpers
     ###
     
-    # Renders a tree of projects as a nested set of unordered lists
+    # Renders the projects as a nested set of unordered lists  (Redmine 0.9.x)
     # The given collection may be a subset of the whole project tree
     # (eg. some intermediate nodes are private and can not be seen)
-    def render_involved_projects_list(projects)
+    # Returns a string containing the HTML for the bleeding-edge projects tab
+    def render_projects_nestedset(projects)
       s = ''
       if projects.any?
         ancestors = []
+        # Iterate over the projects
         projects.each do |project|
           if (ancestors.empty? || project.is_descendant_of?(ancestors.last))
             s << "<ul class='projects #{ ancestors.empty? ? 'root' : nil}'>\n"
@@ -80,23 +82,157 @@ module CollapseApplicationHelperPatch
           end
           classes = (ancestors.empty? ? 'root' : 'child')
           s << "<li>" +
-                       if !@project.nil?
-                link_to_if (project.identifier != @project.identifier), h(project), {:controller => 'projects', :action => 'show', :id => project, :jump => current_menu_item}
-                       else
-                link_to (h(project), {:controller => 'projects', :action => 'show', :id => project})
-			           end
-                ancestors << project
+                      # Only add ':jump => current_menu_item' URL-parameter if on project level
+                      if !@project.nil?
+                        # Only add ':class => selected' URL-parameter if the current project (@project) is equal to project (project)
+                        if (project.identifier == @project.identifier)
+                          link_to (h(project), {:controller => 'projects', :action => 'show', :id => project, :jump => current_menu_item}, :class => 'selected')
+                        else
+                          link_to (h(project), {:controller => 'projects', :action => 'show', :id => project, :jump => current_menu_item})
+                        end
+                      else
+                        link_to (h(project), {:controller => 'projects', :action => 'show', :id => project})
+                      end
+          ancestors << project
         end
         s << ("</li></ul>\n" * ancestors.size)
       end
-      s
+      return s
     end
-
+    
+    # Renders the projects as a tree of unordered lists (Redmine 0.8.x)
+    # Returns a string containing the HTML for the legacy projects tab
+    def render_projects_tree(projects)
+      s = ''
+      if projects.any?
+        s << "<ul>\n"
+        # Iterate over the root-projects
+        projects.keys.sort.each do |root|
+          if jump_to_current_view_implemented == true
+            s << "<li>" +
+                        # Only add ':jump => current_menu_item' URL-parameter if on project level
+                        if !@project.nil?
+                          # Only add ':class => selected' URL-parameter if the current project (@project) is equal to project (project)
+                          if (root.identifier == @project.identifier)
+                            link_to (h(root), {:controller => 'projects', :action => 'show', :id => root, :jump => current_menu_item}, :class => 'selected')
+                          else
+                            link_to (h(root), {:controller => 'projects', :action => 'show', :id => root, :jump => current_menu_item})
+                          end
+                        else
+                          link_to (h(root), {:controller => 'projects', :action => 'show', :id => root})
+                        end
+            s << "</li>\n"
+          else
+            s << "<li>" +
+                        # Only add ':jump => current_menu_item' URL-parameter if on project level
+                        if !@project.nil?
+                          # Only add ':class => selected' URL-parameter if the current project (@project) is equal to project (project)
+                          if (root.identifier == @project.identifier)
+                            link_to (h(root), {:controller => 'projects', :action => 'show', :id => root}, :class => 'selected')
+                          else
+                            link_to (h(root), {:controller => 'projects', :action => 'show', :id => root})
+                          end
+                        else
+                          link_to (h(root), {:controller => 'projects', :action => 'show', :id => root})
+                        end
+            s << "</li>\n"
+          end
+          s << "<ul>\n"
+          # Iterate over the sub-projects
+          projects[root].sort.each do |project|
+            # Skip if the project is a root-project
+            next if project == root
+            if jump_to_current_view_implemented == true
+              s << "<li>" +
+                          # Only add ':jump => current_menu_item' URL-parameter if on project level
+                          if !@project.nil?
+                            # Only add ':class => selected' URL-parameter if the current project (@project) is equal to project (project)
+                            if (project.identifier == @project.identifier)
+                              link_to (h(project), {:controller => 'projects', :action => 'show', :id => project, :jump => current_menu_item}, :class => 'selected')
+                            else
+                              link_to (h(project), {:controller => 'projects', :action => 'show', :id => project, :jump => current_menu_item})
+                            end
+                          else
+                            link_to (h(project), {:controller => 'projects', :action => 'show', :id => project})
+                          end
+              s << "</li>\n"
+            else
+              s << "<li>" +
+                          # Only add ':jump => current_menu_item' URL-parameter if on project level
+                          if !@project.nil?
+                            # Only add ':class => selected' URL-parameter if the current project (@project) is equal to project (project)
+                            if (project.identifier == @project.identifier)
+                              link_to (h(project), {:controller => 'projects', :action => 'show', :id => project}, :class => 'selected')
+                            else
+                              link_to (h(project), {:controller => 'projects', :action => 'show', :id => project})
+                            end
+                          else
+                            link_to (h(project), {:controller => 'projects', :action => 'show', :id => project})
+                          end
+              s << "</li>\n"
+            end
+          end          
+          s << "</ul>\n" # close the UL-element of the sub-projects iteration
+        end
+        s << "</ul>\n" # close the UL-element of the root-projects iteration
+      end
+      return s
+    end
+    
+    # Renders the global menu as an unordered list
+    # Returns a string containing the HTML for the global menu
+    def render_global_menu
+      s = ''
+        s << "<ul>\n"
+          # Global issues link
+          s << "<li>" +
+                      # Add 'alt-selected'-class to the 'a'-element if on global level (!= :controller == issues && != :action == index && != :project_id == nil)
+                      if params[:controller] == 'issues' && params[:action] == 'index' && params[:project_id] == nil
+                        link_to l(:label_issue_view_all), { :controller => 'issues', :id => nil }, :class => 'alt-selected'
+                      else
+                        link_to l(:label_issue_view_all), { :controller => 'issues', :id => nil }
+                      end
+          s << "</li>\n"
+          # Global activity link
+          s << "<li>" +
+                      # Add 'alt-selected'-class to the 'a'-element if on global level (!= :controller == projects && != :action == activity && != :id == nil)
+                      if params[:controller] == 'projects' && params[:action] == 'activity' && params[:id] == nil
+                        link_to l(:label_overall_activity), { :controller => 'projects', :action => 'activity', :id => nil }, :class => 'alt-selected'
+                      else
+                        link_to l(:label_overall_activity), { :controller => 'projects', :action => 'activity', :id => nil }
+                      end
+          s << "</li>\n"
+        s << "</ul>\n"
+      return s
+    end
+    
     # Left-menu tabs renderer
-    # Returns the tabs-variable containing an array of the left-menu tabs to render
+    # Returns an array named 'tabs' containing the left-menu tabs to render
     def left_menu_tabs
+      # Don't render the menus-tab nor the projects-tab unless specifically configured
+      if Setting.plugin_redmine_collapse['show_menus_tab'] == '1' && Setting.plugin_redmine_collapse['show_projects_tab'] == '1'
+        # => Redmine trunk@r2493?
+        if Redmine.const_defined?(:I18n)
+          tabs = [ { :name => 'actions-tab', :label => :label_actions_tab, :partial => 'left_menu/actions.rhtml' },
+                   { :name => 'menus-tab', :label => :label_menus_tab, :partial => 'left_menu/menus.rhtml' },
+                   { :name => 'projects-tab', :label => :label_projects_tab, :partial => 'left_menu/projects.rhtml' } ]
+        else
+          tabs = [ { :name => 'actions-tab', :label => l(:label_actions_tab), :partial => 'left_menu/actions.rhtml' },
+                   { :name => 'menus-tab', :label => l(:label_menus_tab), :partial => 'left_menu/menus.rhtml' },
+                   { :name => 'projects-tab', :label => l(:label_projects_tab), :partial => 'left_menu/projects.rhtml' } ]
+        end
+      # Don't render the menus-tab unless specifically configured
+      elsif Setting.plugin_redmine_collapse['show_menus_tab'] == '1'
+        # => Redmine trunk@r2493?
+        if Redmine.const_defined?(:I18n)
+          tabs = [ { :name => 'actions-tab', :label => :label_actions_tab, :partial => 'left_menu/actions.rhtml' },
+                   { :name => 'menus-tab', :label => :label_menus_tab, :partial => 'left_menu/menus.rhtml' } ]
+        else
+          tabs = [ { :name => 'actions-tab', :label => l(:label_actions_tab), :partial => 'left_menu/actions.rhtml' },
+                   { :name => 'menus-tab', :label => l(:label_menus_tab), :partial => 'left_menu/menus.rhtml' } ]
+        end
       # Don't render the projects-tab unless specifically configured
-      if Setting.plugin_redmine_collapse['show_projects_tab'] == '1'
+      elsif Setting.plugin_redmine_collapse['show_projects_tab'] == '1'
         # => Redmine trunk@r2493?
         if Redmine.const_defined?(:I18n)
           tabs = [ { :name => 'actions-tab', :label => :label_actions_tab, :partial => 'left_menu/actions.rhtml' },
@@ -120,11 +256,16 @@ module CollapseApplicationHelperPatch
     # Left-menu selected-tab based on active controller and activated-tabs settings
     # Returns a string containing the name of the selected tab
     def left_menu_selected_tab
-      # The controllers on which the projects-tab should be activated by default
+    # Initialize an array named "projecttab_controllers" containing the controllers on which the projects-tab should be activated by default
       projecttab_controllers = [ 'welcome', 'account', 'admin', 'users', 'roles', 'trackers', 'issue_statuses', 'workflows', 'custom_fields', 'enumerations', 'settings' ]
       
+      # Activate the projects-tab by default if the projects-tab is activated and the "projecttab_controllers"-array contains the current :controller-param
       if (Setting.plugin_redmine_collapse['show_projects_tab'] == '1' ) && (projecttab_controllers.include?(params[:controller]))
         selectedtab = 'projects-tab'
+      # Activate the menus-tab by default if the menus-tab is activated and the project-menu is shown in the menus-tab
+      elsif (Setting.plugin_redmine_collapse['show_menus_tab'] == '1') && (Setting.plugin_redmine_collapse['show_projectmenu_in_menustab'] == '1')
+        selectedtab = 'menus-tab'
+      # Else activate the actions-tab by default
       else
         selectedtab = 'actions-tab'
       end
