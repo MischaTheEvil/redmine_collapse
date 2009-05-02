d4430db5cf3bc5f066eee74a86d4f14eaa81356f
MischaTheEvil <mischa_the_evil@hotmail.com>
2009-05-02 02:12:07 +0000

    Updated @init.rb@ for release 0.2.0-RC1.
    This commit references issue #249.
    
    
    git-svn-id: svn://evil-dev.net:3690/redmine-collapse/trunk@115 1809a232-8a5f-0410-be6b-a332444e9cb2

--- init.rb
+++ init.rb
@@ -7,21 +7,21 @@ Dispatcher.to_prepare do
   ApplicationHelper.send(:include, CollapseApplicationHelperPatch)
 end
 
-RAILS_DEFAULT_LOGGER.info 'Starting Collapse plugin for Redmine'
+RAILS_DEFAULT_LOGGER.info 'Starting Collapse plugin 0.2.0-RC1 for Redmine'
 
 Redmine::Plugin.register :redmine_collapse do
   name 'Redmine Collapse plugin'
   author 'Mischa The Evil'
-  description 'This is a plugin for Redmine which transforms the static sidebar into a collapsible sidebar'
-  version '0.1.0'
+  description 'This is a plugin for Redmine which transforms the static sidebar into a collapsible sidebar with additional features'
+  version '0.2.0-RC1'
   
   requires_redmine :version_or_higher => '0.8.0'
   
   settings :default => {
-    'position' => '0',
     'show_projects_tab' => '1',
+    'hide_project_selector' => '0',
     'show_menus_tab' => '0',
     'show_projectmenu_in_menustab' => '0',
-    'hide_project_selector' => '0'
+    'position' => '0'
   }, :partial => 'settings/collapse_settings'
 end
